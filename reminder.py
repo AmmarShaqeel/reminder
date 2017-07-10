3a4e542311a5e8b0e8f62f6534b73eca9b9f7eb2
ammar903@gmail.com <ammar903@gmail.com>
2017-07-10 18:35:28 +0800

    basic outline

--- reminder.py
+++ reminder.py
@@ -1,1 +1,21 @@
-    
+import subprocess
+import time
+from datetime import datetime
+
+i = 0
+
+while i < 1:
+ current_hour = datetime.now().hour
+ 
+ if current_hour > 6 and current_hour < 20:
+     print('TK: I would stay on')
+     time.sleep(10)
+
+ elif current_hour == 20 or current_hour > 20:
+    print('TK: I would turn off now')
+    time.sleep(5)
+
+ elif current_hour < 20:
+    print('TK: I would turn off now')
+    time.sleep(5)
+            
